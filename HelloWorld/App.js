a4e83ae938549eb7d092e69cc69a729ddea1d0f9
Lindan Thillanayagam <lindx4@gmail.com>
2018-12-30 16:53:18 -0500

    Made basic text input element

---
+++ HelloWorld/App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, TextInput, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+//type Props = {};
+export default class App extends Component {
+
+  state = {
+    placeName: 'Ditto'
+  }
+
+  render() {  
+    return (
+      <View style={styles.container}>
+        <TextInput 
+        value={this.state.placeName} 
+        onChangeText={val => {this.setState({placeName: val})}}
+        placeholder = "An awesome place"
+        style={{ color: "grey", width: 100, borderColor: "black", borderWidth: 1}}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
